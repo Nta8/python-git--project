21d85457dd9f7b99e30befda7d56117115a2f9fe
Nta8 <68030084@kmitl.ac.th>
2025-08-06 11:18:34 +0700

    refact: change greeting to 'Hi'

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ def say_hello(name):
 
 def say_hello(name):
     now = datetime.datetime.now()
-    print(f"Hello,{name} from {config.APP_NAME}!")
+    print(f"Hi,{name} from {config.APP_NAME}!")
     print(f"Today is {now.strftime('%Y-%m-%d')}")
 
 def greet_user():
